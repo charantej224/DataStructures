1f03b8091253f9a90f107e025f56e596063ff7d3
charantej224 <charantej.career@gmail.com>
2021-06-10 10:46:37 -0400

    heap sort changes

--- datastructures/src/main/java/com/charan/datastructures/trees/HeapSort.java
+++ datastructures/src/main/java/com/charan/datastructures/trees/HeapSort.java
@@ -9,12 +9,13 @@ public class HeapSort {
 
 
     public static void main(String[] args) {
-        //int[] inputArray = {10, 5, 4, 3, 2, 6, 10, 12, 14, 23, 32, 78, 24, 21, 22, 26};
+/*        //int[] inputArray = {10, 5, 4, 3, 2, 6, 10, 12, 14, 23, 32, 78, 24, 21, 22, 26};
         int[] inputArray = {10, 5, 4, 2, 6, 10, 12, 14, 23, 32, 78, 24, 21, 22, 26};
         maxHeapify(inputArray, inputArray.length / 2 - 1, inputArray.length);
         System.out.println(Arrays.toString(inputArray));
         heapSort(inputArray, inputArray.length);
-        System.out.println(Arrays.toString(inputArray));
+        System.out.println(Arrays.toString(inputArray));*/
+        insertHeapValue(400);
     }
 
     private static void maxSwap(int[] inputArray, int i, int upperBoundary) {
@@ -27,7 +28,7 @@ public class HeapSort {
                 index = right;
             } else if (inputArray[left] > inputArray[right] && inputArray[left] > inputArray[i]) {
                 index = left;
-            } else if (inputArray[left] == inputArray[right] && inputArray[left] > inputArray[i]){
+            } else if (inputArray[left] == inputArray[right] && inputArray[left] > inputArray[i]) {
                 index = left;
             }
         } else {
@@ -62,6 +63,64 @@ public class HeapSort {
             inputArray[0] = temp;
             maxHeapify(inputArray, 0, i);
         }
+    }
+
+    public static void insertHeapValue(int insertValue) {
+        int[] inputArray = {10, 5, 4, 2, 6, 10, 12, 14, 23, 32, 78, 24, 21, 22, 26};
+        maxHeapify(inputArray, inputArray.length / 2 - 1, inputArray.length);
+        System.out.println(Arrays.toString(inputArray));
+        int[] modifiedInputArray = new int[inputArray.length + 1];
+        arrayCopy(inputArray, modifiedInputArray);
+        modifiedInputArray[inputArray.length] = insertValue;
+        upshift(modifiedInputArray, modifiedInputArray.length - 1, modifiedInputArray.length);
+        System.out.println(Arrays.toString(modifiedInputArray));
+        deleteHeapValue(modifiedInputArray);
+        System.out.println(Arrays.toString(modifiedInputArray));
+    }
+
+    public static void upshift(int[] inputArray, int inputPlace, int upperBoundary) {
+        int indexPlace = ((inputPlace + 1) / 2) - 1;
+        int left = 2 * indexPlace + 1;
+        int right = 2 * indexPlace + 2;
+        int index = -1, temp;
+
+        if (indexPlace < 0)
+            return;
+
+        if (right < upperBoundary && left < upperBoundary) {
+            if (inputArray[right] > inputArray[left] && inputArray[right] > inputArray[indexPlace]) {
+                index = right;
+            } else if (inputArray[left] > inputArray[right] && inputArray[left] > inputArray[indexPlace]) {
+                index = left;
+            } else if (inputArray[left] == inputArray[right] && inputArray[left] > inputArray[indexPlace]) {
+                index = left;
+            }
+        } else {
+            if (right < upperBoundary && inputArray[right] > inputArray[indexPlace]) {
+                index = right;
+            }
+            if (left < upperBoundary && inputArray[left] > inputArray[indexPlace]) {
+                index = left;
+            }
+        }
+        if (index > 0) {
+            temp = inputArray[index];
+            inputArray[index] = inputArray[indexPlace];
+            inputArray[indexPlace] = temp;
+            upshift(inputArray, indexPlace, upperBoundary);
+        }
+
+    }
+
+    private static void arrayCopy(int[] sourceArray, int[] destinationArray) {
+        for (int i = 0; i < sourceArray.length; i++)
+            destinationArray[i] = sourceArray[i];
+    }
+
+    public static void deleteHeapValue(int[] inputArray) {
+        inputArray[0] = inputArray[inputArray.length - 1];
+        maxSwap(inputArray, 0, inputArray.length - 2);
+
     }
 
     public static void buildCompleteBinaryTree() {
