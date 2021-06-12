4d081fe33a8a07e206997195880f3701a87566cf
charantej224 <charantej.career@gmail.com>
2021-06-11 23:14:52 -0400

    quick sort re-defined

--- datastructures/src/main/java/com/charan/datastructures/sorting/QuickSort.java
+++ datastructures/src/main/java/com/charan/datastructures/sorting/QuickSort.java
@@ -5,40 +5,37 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] inputArray = {22, 4, 14, 7, 15, 10, 27, 12};
+        int[] inputArray = {10, 5, 4, 2, 6, 10, 12, 14, 23, 32, 78, 24, 21, 22, 26};
         quickSort(inputArray, 0, inputArray.length - 1);
         System.out.println(Arrays.toString(inputArray));
     }
 
-    private static void quickSort(int[] inputArray, int low, int high) {
-        int pivotPosition = high;
-        boolean loopFlag = true;
-        int i = 0;
-        int j = high - 1;
-        if (i >= j)
+    public static void quickSort(int[] inputArray, int left, int right) {
+        if (left >= right)
             return;
-        while (i < j) {
-            if (inputArray[i] > inputArray[pivotPosition] && inputArray[j] < inputArray[pivotPosition]) {
-                int temp = inputArray[i];
+        int i = left + 1, j = right;
+        int temp;
+        int pivot = inputArray[left];
+
+
+        while (i <= j) {
+            if (pivot < inputArray[i] && pivot > inputArray[j]) {
+                temp = inputArray[i];
                 inputArray[i] = inputArray[j];
                 inputArray[j] = temp;
                 i++;
                 j--;
-            } else if (inputArray[i] < inputArray[pivotPosition]) {
+            }
+            if (pivot >= inputArray[i]) {
                 i++;
-            } else if (inputArray[j] > inputArray[pivotPosition]) {
+            }
+            if (pivot < inputArray[j]) {
                 j--;
             }
         }
-        int temp = inputArray[i];
-        inputArray[i] = inputArray[pivotPosition];
-        inputArray[pivotPosition] = temp;
-        pivotPosition = i;
-
-        if (low < pivotPosition - 1)
-            quickSort(inputArray, 0, pivotPosition - 1);
-        if (pivotPosition + 1 < high)
-            quickSort(inputArray, pivotPosition + 1, high);
-
+        inputArray[left] = inputArray[j];
+        inputArray[j] = pivot;
+        quickSort(inputArray, left, j - 1);
+        quickSort(inputArray, j + 1, right);
     }
 }
